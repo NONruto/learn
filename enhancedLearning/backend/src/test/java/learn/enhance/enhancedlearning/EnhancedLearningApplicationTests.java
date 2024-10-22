f6eb10ae6a3b55ab998a78fb4a6e314899d8737d
ITzKait0 <ben180701@gmail.com>
2024-10-23 00:16:46 +0200

    test

---
+++ enhancedLearning/backend/src/test/java/learn/enhance/enhancedlearning/EnhancedLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.enhance.enhancedlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnhancedLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
